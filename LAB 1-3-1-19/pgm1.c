c89c4b12d607dcfdea95e93022009951cf7228cf
chandan <17co32@aitdgoa.edu.in>
2019-01-10 09:41:12 +0530

    message

--- LAB 1-3-1-19/pgm1.c
+++ LAB 1-3-1-19/pgm1.c
@@ -1,72 +1,67 @@
 #include<stdio.h>
-void decimal_binary(int );
-void binary_decimal(int );
+#include<stdlib.h>
+#include<math.h>
 
+void decimal_binary(int);
+void binary_decimal(int);
 int main()
 {
-      int n,choice;
-      do
-      {
-         printf("\n Enter the following option");
-         printf("\n2.Binary to decimal");
-         printf("\n1.Decimal to binay");
-         printf("\n3.exit");
-         printf("\nEnter the choice: ");
-         scanf("%d",&choice);
-         switch(choice)
-         {
 
-         case 1: printf("Enter the binary number: ");
-                 scanf("%d",&n);
-                 decimal_binary(n);
-                 break;
-
-         case 2: printf("Enter the decimal number: ");
-                 scanf("%d",&n);
-                 binary_decimal(n);
-                 break;
-        case 3: exit(1);
-                break;
-         default:printf("Invaild choice");
-         }
-      }while(choice!=3);
-
+	int num,ch;
+	do
+	{
+		printf("\n1.Decimal to binary");
+		printf("\n2.Binary to decimal");
+		printf("\n3.exit");
+		printf("\nEnter your choice: ");
+		scanf("%d",&ch);
+		switch(ch)
+		{
+			case 1:printf("Enter the decimal number: ");
+			       scanf("%d",&num);
+			       decimal_binary(num);
+			       break;
+			case 2:printf("Enter the binary number: ");
+			       scanf("%d",&num);
+			       binary_decimal(num);
+			       break;
+			case 3:exit(1);
+			default:printf("Invalid choice!!!");
+		}
+	}while(ch!=4);
+	return(0);
 }
 
 
-     void binary_decimal(int num)
-     {
-           int rem,decimal=0,base=1,n;
-               n=num;
-              while(num!=0)
-            {
-              rem=num%2;
-              decimal=decimal+rem*base;
-              num=num/10;
-              base=base*2;
-            }
-            printf("The decimal equivalent of %d is %d",n,decimal);
+void decimal_binary(int n)
+{
+	int i,j,p[10],num;
+	num=n;//store the original number //
+	for(i=0;n>0;i++)
+        {
+            p[i]=n%2;//remainder
+            n=n/2;        //quotient
+        }
 
+	printf("The binary equivalent of %d is: ",num);
+	for(j=i-1;j>=0;j--)
+	 printf("%d",p[j]);
+	 printf("\n\n");
+}
+
+
+
+void binary_decimal(int n)
+{
+    int sum=0,binary,base=1,rem,num;
+    num=n;
+    while(n!=0)
+    {
+        rem=n%10;
+        sum=sum+rem*base;
+        n=n/10;
+        base=base*2;
     }
-
-
-
- void decimal_binary(int n)
- {
-      int i,j,p[10],num;
-      num=n;
-
-      for(i=0;n>0;i++)
-      {
-           p[i]=n%2;
-           n=n/10;
-      }
-       printf("The binary equivalent of %d: ",num);
-        for(j=i-1;j>=0;j--)
-         printf("%d",p[j]);
-
-
-
- }
-
-
+    printf("Decimal equivalent of the binary number %d is:%d",num,sum);
+    printf("\n\n");
+}
